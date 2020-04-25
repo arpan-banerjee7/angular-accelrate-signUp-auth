d292118655bdd6a84185521173d470b8a2e4d960
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-04-25 18:14:35 +0530

    added auto login and auto logout feature

--- src/app/auth-service/auth.service.ts
+++ src/app/auth-service/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { throwError } from "rxjs";
+import { throwError, BehaviorSubject } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
+import { User } from "../model/user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData {
   kind: string;
@@ -15,25 +17,23 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  /*using behavioural subject to store the response after logging in
+  this response data is used for managing the state of the application after the user 
+  has logged in
+  initial value of subject is null ->signifies there is no user data stored,
+  hence user has not logged in.
+  We have used behavioural subjects here ,a s we can get access to a previously
+  emitted observable(which in this case is user respone details when the user logs in)
+  even if have not subscribed to it at  the time when the observable was emitted*/
+  user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]",
-        {
-          email: email,
-          password: password,
-          returnSecureToken: true,
-        }
-      )
-      .pipe(catchError(this.handleError));
-  }
-
-  login(email: string, password: string) {
-    return this.http
-      .post<AuthResponseData>(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]",
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[Api Key]",
         {
           email: email,
           password: password,
@@ -41,23 +41,37 @@ export class AuthService {
         }
       )
       .pipe(
-        catchError((errorRes) => {
-          let errorMessage = "An unknown error occurred!";
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case "EMAIL_EXISTS":
-              errorMessage = "This email exists already";
-              break;
-            case "EMAIL_NOT_FOUND":
-              errorMessage = "This email does not exist.";
-              break;
-            case "INVALID_PASSWORD":
-              errorMessage = "This password is not correct.";
-              break;
-          }
-          return throwError(errorMessage);
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[Api Key]",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
         })
       );
   }
@@ -79,5 +93,73 @@ export class AuthService {
         break;
     }
     return throwError(errorMessage);
+  }
+
+  //using the logged in user/ managing the state after logging in
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    console.log(user);
+    this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem("userData", JSON.stringify(user));
+  }
+
+  /*AUTO LOG IN
+  gets data from persistant storage and emits a new observable
+    with the earlier logged in user(before refreshing the page)*/
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem("userData"));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    /*checking if the loaded user has a valid token or not
+    (basically if it has expired or not). We will call this in our app-component
+    as thats the first thing which gets loaded when the app reloads, 
+    thereby emitting an observable with a new user object whcih has the value 
+    of the last logged in user*/
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  /*AUTO LOG OUT
+  use settimeout and clearing the local storage and settimeout 
+  to implement the auto log out functionality as soon as the token expires.
+  */
+  logout() {
+    this.user.next(null);
+    localStorage.removeItem("userData");
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { AuthService } from "../auth-service/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.css"],
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, OnDestroy {
+  isAuthenticated: Boolean = false;
+  //managing custom observables and subscriptions
+  private userSub: Subscription;
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    /*whenever user logs in/logs out our subject emits
+    an observable*/
+    this.userSub = this.authService.user.subscribe((user) => {
+      /*if there is a user object(means the user is logged in)
+       then set it to true(using truthy/falsey values)*/
+      this.isAuthenticated = !!user;
+    });
   }
-
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+  logOut() {
+    this.authService.logout();
+  }
 }
